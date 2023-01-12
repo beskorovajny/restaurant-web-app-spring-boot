0faa28743e216f692889edf07c5e70f05be3cad7
Andrii Bezkorovainyi <beskorovajny@gmail.com>
2023-01-12 20:47:04 +0200

    [RWA-SB-0004] Open browser on startup event listener created

--- src/main/java/com/app/web/restaurant/RestaurantWebAppApplication.java
+++ src/main/java/com/app/web/restaurant/RestaurantWebAppApplication.java
@@ -1,9 +1,45 @@
 package com.app.web.restaurant;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+@Slf4j
 @SpringBootApplication
 public class RestaurantWebAppApplication {
-	public static void main(String[] args) {SpringApplication.run(RestaurantWebAppApplication.class, args);}
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantWebAppApplication.class, args);
+
+
+	}
+
+	@EventListener({ApplicationReadyEvent.class})
+	public void applicationReadyEvent() {
+		log.info("Application started ... launching browser now");
+		browse("http://localhost:8080/");
+	}
+
+	public static void browse(String url) {
+		if(Desktop.isDesktopSupported()){
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URI(url));
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}else{
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
